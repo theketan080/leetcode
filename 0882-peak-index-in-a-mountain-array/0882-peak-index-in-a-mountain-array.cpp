6ed71e716e2b2800e58d3f8719e32a9b4fb35fd1
ketan080 <contactketan080@gmail.com>
2025-05-24 20:49:53 +0530

    Time: 0 ms (100%), Space: 63.7 MB (12.17%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -4,9 +4,8 @@ public:
         int n = arr.size();
         int s = 0;
         int e = n - 1;
-        int mid = s + (e-s) / 2;
+        int mid = s + (e-s)/2;
         int ans = -1;
-
 
         while(s < e){
             if(arr[mid] < arr[mid + 1]){
@@ -16,7 +15,7 @@ public:
                 ans = mid;
                 e = mid;
             }
-            mid = s + (e-s) / 2;
+            mid = s + (e-s)/2;
         }
 
         return ans;
