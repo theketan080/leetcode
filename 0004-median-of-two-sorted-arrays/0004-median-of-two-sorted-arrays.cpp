a4d83dff992a230c17065b5b185162e2e89d2f02
ketan080 <contactketan080@gmail.com>
2025-07-11 17:57:39 +0530

    Time: 0 ms (100%), Space: 95.1 MB (59.74%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,54 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // Ensure nums1 is the smaller array to minimize binary search operations
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
         int m = nums1.size();
         int n = nums2.size();
-        int total = m + n;
-        int target = (total + 1) / 2;
-        
-        if (m > n) return findMedianSortedArrays(nums2, nums1);
-        
         int left = 0, right = m;
+        int halfLen = (m + n + 1) / 2;
+        
         while (left <= right) {
-            int partition1 = (left + right) / 2;
-            int partition2 = target - partition1;
+            int i = (left + right) / 2;  // Partition position in nums1
+            int j = halfLen - i;          // Partition position in nums2
             
-            int maxLeft1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
-            int minRight1 = (partition1 == m) ? INT_MAX : nums1[partition1];
-            
-            int maxLeft2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
-            int minRight2 = (partition2 == n) ? INT_MAX : nums2[partition2];
-            
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                if (total % 2 == 0) {
-                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0;
-                } else {
-                    return max(maxLeft1, maxLeft2);
-                }
-            } else if (maxLeft1 > minRight2) {
-                right = partition1 - 1;
+            if (i < m && nums2[j-1] > nums1[i]) {
+                // i is too small, need to increase it
+                left = i + 1;
+            } else if (i > 0 && nums1[i-1] > nums2[j]) {
+                // i is too big, need to decrease it
+                right = i - 1;
             } else {
-                left = partition1 + 1;
+                // i is perfect, calculate median
+                int maxLeft;
+                if (i == 0) {
+                    maxLeft = nums2[j-1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i-1];
+                } else {
+                    maxLeft = max(nums1[i-1], nums2[j-1]);
+                }
+                
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+                
+                int minRight;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = min(nums1[i], nums2[j]);
+                }
+                
+                return (maxLeft + minRight) / 2.0;
             }
         }
+        
         return 0.0;
     }
 };
