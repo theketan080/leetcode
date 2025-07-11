6cf4e6b1e48f4d6db2a3333925608f35faf16398
ketan080 <contactketan080@gmail.com>
2025-07-11 17:55:13 +0530

    Time: 0 ms (100%), Space: 95.1 MB (78.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,54 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-   
-    if (nums1.size() > nums2.size()) {
-        return findMedianSortedArrays(nums2, nums1);
-    }
-
-    int m = nums1.size(), n = nums2.size();
-    int low = 0, high = m;
-
-    while (low <= high) {
-        int partitionX = (low + high) / 2;
-        int partitionY = (m + n + 1) / 2 - partitionX;
-
-        int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
-        int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
-
-        int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
-        int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY];
-
-        if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-            if ((m + n) % 2 == 0) {
-                return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
-            } else {
-                return max(maxLeftX, maxLeftY);
-            }
-        } else if (maxLeftX > minRightY) {
-            high = partitionX - 1;
-        } else {
-            low = partitionX + 1;
+        // Ensure nums1 is the smaller array to minimize binary search operations
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
+        
+        int m = nums1.size();
+        int n = nums2.size();
+        int left = 0, right = m;
+        int halfLen = (m + n + 1) / 2;
+        
+        while (left <= right) {
+            int i = (left + right) / 2;  // Partition position in nums1
+            int j = halfLen - i;          // Partition position in nums2
+            
+            if (i < m && nums2[j-1] > nums1[i]) {
+                // i is too small, need to increase it
+                left = i + 1;
+            } else if (i > 0 && nums1[i-1] > nums2[j]) {
+                // i is too big, need to decrease it
+                right = i - 1;
+            } else {
+                // i is perfect, calculate median
+                int maxLeft;
+                if (i == 0) {
+                    maxLeft = nums2[j-1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i-1];
+                } else {
+                    maxLeft = max(nums1[i-1], nums2[j-1]);
+                }
+                
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+                
+                int minRight;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = min(nums1[i], nums2[j]);
+                }
+                
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        
+        return 0.0;
     }
-
-    
-    throw invalid_argument("Input arrays are not sorted or invalid.");
-}
 };
