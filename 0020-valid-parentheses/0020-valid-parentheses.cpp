92d56c9adb29b6874b62986b99a2111b9d826165
ketan080 <contactketan080@gmail.com>
2025-08-17 13:48:54 +0530

    Time: 0 ms (100%), Space: 8.8 MB (60.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                st.push(s[i]);
-            }else if(!st.empty()){
-                if(s[i] == ')' && st.top() == '('){
-                    st.pop();
-                }else if(s[i] == '}' && st.top() == '{'){
-                    st.pop();
-                }else if(s[i] == ']' && st.top() == '['){
-                    st.pop();
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s[i];
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(!st.empty()){
+                    if(ch == ')' && st.top() == '('){
+                        st.pop();
+                    }
+                    else if(ch == '}' && st.top() == '{'){
+                        st.pop();
+                    }
+                    else if(ch == ']' && st.top() == '['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
                 }
                 else{
                     return false;
@@ -22,9 +31,9 @@ public:
 
         if(st.empty()){
             return true;
-        }else{
+        }
+        else{
             return false;
         }
-
     }
 };
