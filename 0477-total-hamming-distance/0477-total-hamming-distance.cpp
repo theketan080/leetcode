6d5a4eab21025e3391fbe94abebd838d3371d691
ketan080 <contactketan080@gmail.com>
2025-08-22 14:54:00 +0530

    Time: 8 ms (73.08%), Space: 22.9 MB (51.32%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+
+        // 32 bits for int
+        for (int bit = 0; bit < 32; bit++) {
+            int ones = 0;
+            for (int num : nums) {
+                ones += (num >> bit) & 1;
+            }
+            int zeros = n - ones;
+            ans += ones * zeros;
+        }
+
+        return ans;
+    }
+};
