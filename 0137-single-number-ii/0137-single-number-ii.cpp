5d50a26621faec04256b096d5f6df05642fc75cf
ketan080 <contactketan080@gmail.com>
2025-08-23 15:07:51 +0530

    Time: 3 ms (33.26%), Space: 13.3 MB (86.41%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            int zeroCount = 0;
+            int oneCount = 0;
+            for(int j = 0; j < n; j++){
+                if(nums[j] & (1<<i)==0)zeroCount++;
+                if(nums[j] & (1<<i))oneCount++;
+            }
+
+            if(oneCount % 3){
+                ans = (ans | (1<<i));
+            }        
+        }
+
+        return ans;
+    }
+};
