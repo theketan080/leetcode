e362939e7cc286dcedac63770c233d777107e0b7
ketan080 <contactketan080@gmail.com>
2025-09-04 22:02:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        long long sum = 0;
-        
-        //subarray length is equal to k
-        for(int i = 0; i < k; i++){
-            sum += nums[i];
-        }
-
-        double avg = (double)sum / k;
-        double maxAvg = avg;
-        //max avg value 
+        /*
+        window length = k;
+        jb window lengt purra ho tb computation kro
+        computation avg niakalna hai
+        agr window ka size bdh ke k se jyada ho gya toh
+        piche se window shrink krenge yaani sum mein se gthayenge
+        agr window ka size chhota hai k se toh sirf jodo elements ko taaki woo
+        avg jb condition meet ho tb nikal pye;
+        */
         int i = 0;
-        int j = k;
-        while(j < nums.size()){
-            sum += nums[j];
+        int j = 0;
+        double avg = 0;
+        double sum = 0;
+        double maxAvg = INT_MIN;
 
-            if(j-i+1 > k){
+        while (j < nums.size()) {
+            sum += nums[j];
+            if (j - i + 1 > k) {
                 sum -= nums[i];
                 i++;
             }
-            if(j-i+1 == k){
-                avg = (double)sum/k;
-                maxAvg = max(maxAvg,avg);
+
+            if (j - i + 1 == k) {
+                avg = sum / k;
             }
+            maxAvg = max(maxAvg, avg);
 
             j++;
         }
