481f32089733ca98cf5fcf4c8aa7b224b7531d3e
ketan080 <contactketan080@gmail.com>
2025-09-05 18:52:11 +0530

    Time: 3 ms (25.87%), Space: 8.7 MB (37.18%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-       int n = s.length();
-       unordered_map<char,int>mp;
-       int i = 0;
-       int j = 0;
-       int count = 0;
+      int n = s.length();
+      /*
+      xyzzaz 
+      window x
+      window y
+      window z size 3 computation
+      wpis jb z pe aye toh hmare window ke andr abhi ek z hai already toh usse pehle htayenge
+      toh ek loop chlayenge jisme hm i ko tbtk bdhayenge jbtk woo z ke count ko 1 nhi kr deta
+      */
 
-       while(j < n){
+      unordered_map<char,int>mp;
+
+      int i = 0;
+      int j = 0;
+      int count = 0;
+
+      while(j < n){
+
         mp[s[j]]++;
 
-        while(j-i+1 > 3){
-           
-                mp[s[i]]--;
-                if(mp[s[i]] == 0){
-                    mp.erase(s[i]);
-                }
-                i++;
-            
-        }
-        if(j-i+1 == 3){
-            if(mp.size() == 3){
-                count++;
-            }
+        while(mp[s[j]] > 1){
+           mp[s[i]]--;
+           if(mp[s[i]] == 0)mp.erase(s[i]);
+           i++;
         }
 
+        if(j - i + 1 > 3){
+            mp[s[i]]--;
+            if(mp[s[i]] == 0)mp.erase(s[i]);
+            i++;
+        }
+       
+
+        if(j-i+1 == 3 && mp.size() == 3){
+            count++;
+        }
         j++;
+      }
 
-
-       }
-
-
-       return count;
+      return count;
     }
 };
