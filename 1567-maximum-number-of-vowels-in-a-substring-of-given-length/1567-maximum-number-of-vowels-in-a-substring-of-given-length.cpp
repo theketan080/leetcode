bbee353335d742d0fc9404a04c4d5bae2f040654
ketan080 <contactketan080@gmail.com>
2025-09-08 19:47:37 +0530

    Time: 11 ms (32.79%), Space: 13.2 MB (34.94%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
+    bool isVowel(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            return true;
+
+        return false;
+    }
+
     int maxVowels(string s, int k) {
         int n = s.length();
+        int maxVowel = 0;
+        int count = 0;
+
+        for (int i = 0; i < k - 1; i++) {
+            if (isVowel(s[i]))
+                count++;
+        }
+        maxVowel = count;
 
         int i = 0;
-        int j = 0;
-        int count = 0;
-        int maxCount = INT_MIN;
-        while (j < n) {
+        int j = k-1;
 
-            if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' ||
-                s[j] == 'u') {
+        while (j < s.length()) {
+            if (isVowel(s[j]))
                 count++;
-            }
 
-            if (j - i + 1 > k) {
-                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-                    s[i] == 'u') {
+            maxVowel = max(maxVowel, count);
+
+            if (j - i + 1 >= k) {
+                if (isVowel(s[i]))
                     count--;
-                }
                 i++;
             }
-            if (j - i + 1 == k) {
-                maxCount = max(maxCount, count);
-            }
-
             j++;
         }
 
-        return maxCount;    
+        return maxVowel;
     }
 };
