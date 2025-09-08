92738af33b7eabd5b9926891c1a1f31eae519a11
ketan080 <contactketan080@gmail.com>
2025-09-08 15:12:09 +0530

    Time: 9 ms (40.71%), Space: 10.7 MB (25.27%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.length();
-        int n2 = s2.length();
+        int m = s1.length();
+        int n = s2.length();
+        unordered_map<char,int>mp;
+        unordered_map<char,int>cp;
+
+        for(char ch:s1){
+            mp[ch]++;
+        }
 
         int i = 0;
         int j = 0;
 
-        unordered_map<char,int>mp1;
-        unordered_map<char,int>mp2;
-
-        for(auto &it:s1){
-            mp1[it]++;
+        while(j < s2.length()){
+            cp[s2[j]]++;
+        if(j-i+1 > m){
+            cp[s2[i]]--;
+            if(cp[s2[i]] == 0) cp.erase(s2[i]);
+            i++;
         }
 
-        while(j < n2){
-            mp2[s2[j]]++;
-
-            if(j-i+1 > n1){
-                mp2[s2[i]]--;
-                if(mp2[s2[i]] == 0){
-                    mp2.erase(s2[i]);
-                }
-                i++;
+        if(j-i+1 == m){
+            if(mp == cp){
+                return true;
             }
+        }
 
-            if(j-i+1 == n1){
-                if(mp1 == mp2){
-                    return true;
-                }
-            }
-
-            j++;
+        j++;
         }
 
         return false;
-
     }
 };
