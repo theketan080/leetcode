61a06620a83534a229a6c06ba9cb4d6b7469cfc4
ketan080 <contactketan080@gmail.com>
2025-09-08 13:04:21 +0530

    Time: 18 ms (31.06%), Space: 14.9 MB (23.42%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int m = s.length();
-        int n = p.length();
-
         unordered_map<char, int> mp;
         unordered_map<char, int> cp;
-        vector<int> ans;
-
-        for (char i : p) {
-            mp[i]++;
-        }
-
         int i = 0;
         int j = 0;
+        vector<int> ans;
+        for (char ch : p) {
+            mp[ch]++;
+        }
 
         while (j < s.length()) {
-            cp[s[j]]++;
-
-            if (j - i + 1 > n) {
+        
+                cp[s[j]]++;
+            
+            if (j - i + 1 > p.length()) {
                 cp[s[i]]--;
                 if (cp[s[i]] == 0)
                     cp.erase(s[i]);
                 i++;
             }
+            if (mp == cp && j - i + 1 == p.length()) {
+                ans.push_back(i);
+            }
 
-           if(j - i + 1 == n){
-            if(cp == mp) ans.push_back(i);
-           }
 
             j++;
         }
