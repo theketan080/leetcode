bcb737205d1b2f30254801e0d7f338137d378e20
ketan080 <contactketan080@gmail.com>
2025-09-19 13:09:33 +0530

    Time: 8 ms (42.27%), Space: 10.8 MB (24.7%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,6 +3,7 @@ public:
     bool checkInclusion(string s1, string s2) {
         int m = s1.length();
         int n = s2.length();
+
         unordered_map<char,int>mp;
         unordered_map<char,int>cp;
 
@@ -13,21 +14,21 @@ public:
         int i = 0;
         int j = 0;
 
-        while(j < s2.length()){
+        while(j < n){
             cp[s2[j]]++;
-        if(j-i+1 > m){
-            cp[s2[i]]--;
-            if(cp[s2[i]] == 0) cp.erase(s2[i]);
-            i++;
-        }
 
-        if(j-i+1 == m){
-            if(mp == cp){
-                return true;
+            if(j-i+1 > m){
+                cp[s2[i]]--;
+                if(cp[s2[i]] == 0)cp.erase(s2[i]);
+                i++;
             }
-        }
 
-        j++;
+            if(j-i+1 == m){
+               if(mp==cp){
+                return true;
+               }
+            }
+            j++;
         }
 
         return false;
