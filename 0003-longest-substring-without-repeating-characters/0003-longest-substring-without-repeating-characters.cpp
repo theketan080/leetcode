6b5dc2703d2896a76f45063fe0e107d5e011de20
ketan080 <contactketan080@gmail.com>
2025-09-26 13:43:13 +0530

    Time: 7 ms (65.6%), Space: 12.4 MB (40.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        /*
-        hm sbse pehle first letter pe ayenge and usko hm map ke andr daalenge
-        quki woo hmare window ke andr hai. fir hm aage bdhenge but daalne se
-        pehle yeh check krenge kya woo hmare map mein already hai toh nhi agr
-        hai toh pehle usko htayenge fir aage bdhenge
-        */
-        unordered_map<char, int> mp;
+        int n = s.length();
+        unordered_map<char,int>mp;
+
         int i = 0;
         int j = 0;
-        int maxLength = INT_MIN;
-        while (j < s.length()) {
+        int maxLen = INT_MIN;
+        if(s.empty()){
+            return 0;
+        }
+        while(j < n){
             mp[s[j]]++;
+
             while(mp[s[j]] > 1){
                 mp[s[i]]--;
                 if(mp[s[i]] == 0) mp.erase(s[i]);
-                i++;
+                i++; 
             }
-            maxLength = max(maxLength, j - i + 1);
+
+            maxLen = max(maxLen, (int)mp.size());
             j++;
         }
-        return maxLength== INT_MIN ? 0: maxLength ;
+
+        return maxLen;
     }
 };
