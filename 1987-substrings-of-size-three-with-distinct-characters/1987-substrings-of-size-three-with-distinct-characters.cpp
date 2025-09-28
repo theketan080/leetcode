627fee642786ba580d3769d4631f9237a8f7153a
ketan080 <contactketan080@gmail.com>
2025-09-28 22:33:38 +0530

    Time: 2 ms (35.02%), Space: 8.8 MB (31.12%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -4,24 +4,30 @@ public:
         int n = s.length();
 
         unordered_map<char,int>mp;
+        int count = 0;
+
         int i = 0;
         int j = 0;
-        int count = 0;
 
         while(j < n){
             mp[s[j]]++;
 
-                while(j-i+1 > 3 || mp[s[j]] > 1){
-                    mp[s[i]]--;
-                    if(mp[s[i]] == 0)mp.erase(s[i]);
-                    i++;
-                }
+            if(j-i+1 > 3){
+                mp[s[i]]--;
+                if(mp[s[i]] == 0)mp.erase(s[i]);
+                i++;
+            }
+            while(mp[s[j]] > 1){
+                mp[s[i]]--;
+                if(mp[s[i]] == 0) mp.erase(s[i]);
+                i++;
+            }
+
             if(j-i+1 == 3 && mp.size() == 3){
                 count++;
             }
             j++;
         }
-
         return count;
     }
 };
