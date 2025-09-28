b95bce3bf39af5b8155c4164d5eafc8acc66cc2a
ketan080 <contactketan080@gmail.com>
2025-09-28 22:59:53 +0530

    Time: 28 ms (18.97%), Space: 14.9 MB (22.69%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char, int> mp;
-        unordered_map<char, int> cp;
-        int i = 0;
-        int j = 0;
-        vector<int> ans;
-        for (char ch : p) {
-            mp[ch]++;
+        int m = s.length();
+        int n = p.length();
+
+        unordered_map<char,int>mp;
+        unordered_map<char,int>cp;
+
+        for(int i = 0; i < n; i++){
+            mp[p[i]]++;
         }
 
-        while (j < s.length()) {
-        
-                cp[s[j]]++;
-            
-            if (j - i + 1 > p.length()) {
-                cp[s[i]]--;
-                if (cp[s[i]] == 0)
-                    cp.erase(s[i]);
-                i++;
+        int i = 0;
+        int j = 0;
+        vector<int>ans;
+
+        while(j < m){
+            cp[s[j]]++;
+
+            if(j-i+1 > n){
+                    cp[s[i]]--;
+                    if(cp[s[i]]==0)cp.erase(s[i]);
+                    i++;
             }
-            if (mp == cp && j - i + 1 == p.length()) {
+
+            if(j-i+1 == n && mp == cp){
                 ans.push_back(i);
             }
 
