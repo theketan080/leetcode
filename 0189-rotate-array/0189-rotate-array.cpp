40459df43287aadeef57d2c7ea56a5039d6215c4
ketan080 <contactketan080@gmail.com>
2026-06-19 11:00:31 +0530

    Time: 0 ms (100%), Space: 30.5 MB (18.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         vector<int>ans(n);
 
-        for(int i = 0; i < n; i++ ){
+        for(int i = 0; i < n; i++){
             ans[(i+k)%n] = nums[i];
         }
 
